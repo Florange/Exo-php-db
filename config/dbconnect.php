892b0d33f770c0959ace5881ff4802dc8030e2e7
Florange <f.gobat@hotmail.com>
2018-02-06 16:16:08 +0100

    exe 2

--- config/dbconnect.php
+++ config/dbconnect.php
@@ -1,28 +1,19 @@
 <?php
 Class Db{
-    private static $_connect;
     
-    public static function connect()
-    {
-        
-    if( !isset( self::$_connect ) )
-    {
-        self::$_connect = new mysqli( DB_HOST, DB_USER, DB_MDP, DB_SELECT);
-    }
-    return self::$_connect;
-    }
-}
- 
-
-$db = Db::connect();
-$results = $db->query( 'SELECT * FROM articles' );
-
-
-//var_dump ( $results );
-
-
-while( $row = $results->fetch_object() )
+private static $_connect;
+public static function connect()
 {
-    echo '<pre>',var_dump($row),'</pre';
-    echo $row->TitleArticle;
+    
+if( !isset( self::$_connect ) )
+{
+$config = parse_ini_file( SITE_PATH . '/includes/config.ini' );
+self::$_connect = new mysqli( $config['dbhost'], $config['dbuser'],
+$config['dbpass'], $config['dbname'],
+$config['dbport'] );
+}
+
+return self::$_connect;
+}
+    
 }
